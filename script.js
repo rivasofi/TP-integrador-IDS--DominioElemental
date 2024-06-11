79044b407d3142d9c1f2facd7e54ae04785c5bc8
notconra <cgarciap@fi.uba.ar>
2024-06-10 22:03:45 -0300

    inicializacion de la pagina de login con funcionamiento y animaciones

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('form-open').addEventListener('click', function() {
+    var loginForm = document.querySelector('.login_form');
+    if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'block';
+    } else {
+        loginForm.style.display = 'none';
+    }
+});
