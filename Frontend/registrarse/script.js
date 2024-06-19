e41700744d2bd45629ac896c43f7d9c4d1480759
sofi <srivas@fi.uba.ar>
2024-06-19 17:44:17 -0300

    Se sumaron los términos y condiciones del juego

--- Frontend/registrarse/script.js
+++ Frontend/registrarse/script.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var boton_terminos_condiciones = document.getElementById('terminos_cond_boton');
-    var div_terminos_condiciones = document.getElementById('terminos');
+    var popup_terminos_condiciones = document.getElementById('terminos_popup');
+    var close_popup = document.getElementById('close_popup');
+    var accept_terms = document.getElementById('accept_terms');
     var formulario_registro = document.querySelector('form');
     var checkbox_aceptar_terminos = document.querySelector('input[type="checkbox"]');
     var contrasena = document.getElementById('password');
@@ -8,18 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
     boton_terminos_condiciones.addEventListener('click', function (evento) {
         evento.preventDefault();
-        if (div_terminos_condiciones.style.display === 'none' || div_terminos_condiciones.style.display === '') {
-            div_terminos_condiciones.style.display = 'block';
-        } else {
-            div_terminos_condiciones.style.display = 'none';
+        popup_terminos_condiciones.style.display = 'block';
+    });
+
+    close_popup.addEventListener('click', function () {
+        popup_terminos_condiciones.style.display = 'none';
+    });
+
+    accept_terms.addEventListener('click', function () {
+        popup_terminos_condiciones.style.display = 'none';
+        checkbox_aceptar_terminos.checked = true;
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target == popup_terminos_condiciones) {
+            popup_terminos_condiciones.style.display = 'none';
         }
     });
-    formulario_registro.addEventListener('submit', function(evento) {
+
+    formulario_registro.addEventListener('submit', function (evento) {
         if (!checkbox_aceptar_terminos.checked) {
-            alert('debes aceptar los terminos y condiciones');
+            alert('Debes aceptar los términos y condiciones');
             evento.preventDefault();
-        }if (contrasena.value !== confirmar_contra.value) {
-            alert('las contraseñas no son iguales');
+        } else if (contrasena.value !== confirmar_contra.value) {
+            alert('Las contraseñas no son iguales');
             evento.preventDefault();
         }
     });
