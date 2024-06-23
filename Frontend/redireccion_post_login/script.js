01164fc4a214d9cee50e63d74be50ed829532e05
notconra <cgarciap@fi.uba.ar>
2024-06-23 02:07:33 -0300

    creacion de carpeta recursos_multimedia, archivos como el fondo y la musica, asimismo hice la base del html y copie el diseno de la pagina principal en el css para que sea legible para el usuario

---
+++ Frontend/redireccion_post_login/script.js
@@ -0,0 +1,22 @@
+document.body.addEventListener('click', function () {
+    reproducir_audio();
+});
+
+function reproducir_audio() {
+    var audio = document.getElementById("background-audio");
+    audio.play();
+}
+
+function mutear_audio() {
+    var audio = document.getElementById("background-audio");
+    audio.muted = !audio.muted;
+    var icono = document.querySelector('.boton_volumen i');
+
+    if (audio.muted) {
+        icono.classList.remove('bx-volume-full');
+        icono.classList.add('bx-volume-mute');
+    } else {
+        icono.classList.remove('bx-volume-mute');
+        icono.classList.add('bx-volume-full');
+    }
+}
