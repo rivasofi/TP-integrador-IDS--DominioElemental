692e6cb6016d25e8d8ae04c391914c0e2167830c
notconra <cgarciap@fi.uba.ar>
2024-07-02 18:35:07 -0300

    creacion de tabla de usuarios en python

--- Backend/modelos.py
+++ Backend/modelos.py
@@ -2,6 +2,13 @@ from flask_sqlalchemy import CheckConstraint
 from flask_sqlalchemy import SQLAlchemy
 from app import db
 
+class Usuario(db.Model):
+    __tablename__= 'usuarios'
+    id = db.Column(db.Integer,primary_key=True)
+    nombre= db.Column(db.string(255),nulltable=False)
+    plata= db.Column(db.integer,nulltable=False)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
+
 class Carta(db.Model):
     __tablename__ = 'cartas'
     id = db.Column(db.Integer, primary_key=True)
@@ -20,9 +27,3 @@ class Registro(db.Model):
     __table_args__ = (
         CheckConstraint('confirmar_contraseña = contraseña', name='chk_confirmacion_contraseña'),
     )
-
-
-
-
-
-
