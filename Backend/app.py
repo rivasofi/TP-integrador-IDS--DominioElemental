2ea0a1535a5ce403a34d825fe49f83fe407a71bd
sofi <srivas@fi.uba.ar>
2024-07-07 20:42:51 -0300

    Modifico el back para que la tienda sea funcional y actualice la BDD al comprar

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify, send_from_directory, request
+from flask import Flask, jsonify, send_from_directory, request, flash
 from config import SQLALCHEMY_DATABASE_URI
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_cors import CORS
 from models import db, Carta, Usuario, CartasUsuario
 import os
+from sqlalchemy.sql.expression import func
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
@@ -47,6 +49,58 @@ def submit():
 
     return '¡Datos guardados con éxito!'
 
+@app.route('/comprar', methods=['POST'])
+def comprar_carta():
+    data = request.json
+    nombre_producto = data.get('nombre')
+    precio = data.get('precio')
+
+    usuario = Usuario.query.filter_by(nombre='Demo').first()
+
+    if usuario:
+        if usuario.plata >= precio:
+            usuario.plata -= precio
+            
+            elemento = ''
+            if nombre_producto == 'Pack Hielo':
+                elemento = 'nieve'
+            elif nombre_producto == 'Pack Fuego':
+                elemento = 'fuego'
+            elif nombre_producto == 'Pack Agua':
+                elemento = 'agua'
+            elif nombre_producto == 'Pack Tierra':
+                elemento = 'tierra'
+            elif nombre_producto == 'Pack Aleatorio':
+                elementos = ['nieve', 'fuego', 'agua', 'tierra']
+                elemento = random.choice(elementos)
+
+            cartas = Carta.query.filter_by(elemento=elemento).order_by(func.random()).limit(4).all()
+            
+            dinero_adicional = 0
+            for carta in cartas:
+                cartas_usuario_entry = CartasUsuario.query.filter_by(usuario_id=usuario.id, carta_id=carta.id).first()
+                if cartas_usuario_entry:
+                    if cartas_usuario_entry.cantidad == 1:
+                        usuario.plata += 2
+                        dinero_adicional += 2
+                    else:
+                        cartas_usuario_entry.cantidad += 1
+                else:
+                    cartas_usuario_entry = CartasUsuario(usuario_id=usuario.id, carta_id=carta.id, cantidad=1)
+                    db.session.add(cartas_usuario_entry)
+            
+            db.session.commit()
+            
+            mensaje = f'Compra de {nombre_producto} realizada con éxito.'
+            if dinero_adicional > 0:
+                mensaje += f' Se te sumó ${dinero_adicional} por cartas repetidas.'
+
+            return jsonify({'mensaje': mensaje, 'saldo': usuario.plata}), 200
+        else:
+            return jsonify({'error': 'Saldo insuficiente para realizar la compra.'}), 400
+    else:
+        return jsonify({'error': 'Usuario no encontrado.'}), 404
+    
 if __name__ == '__main__':
     
     with app.app_context():
