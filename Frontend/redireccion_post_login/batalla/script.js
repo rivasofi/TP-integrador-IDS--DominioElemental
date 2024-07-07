0c84b24263e6968f98795618ea5fd351c988d485
notconra <cgarciap@fi.uba.ar>
2024-07-07 02:04:53 -0300

    cambio de nombres variables javascript

--- Frontend/redireccion_post_login/batalla/script.js
+++ Frontend/redireccion_post_login/batalla/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 document.addEventListener("DOMContentLoaded", function() {
     const items = document.querySelectorAll('.item_grid');
-    const images = {
+    const imagenes = {
         archivo1: [
             "../../recursos_multimedia/imagenes_login/login_4.png",
             "../../recursos_multimedia/imagenes_login/login_6.png"
@@ -30,12 +30,12 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     items.forEach(item => {
-        const gameName = item.getAttribute('href').split('/').pop().replace('.html', '');
-        let currentIndex = 0;
+        const titulo_juego = item.getAttribute('href').split('/').pop().replace('.html', '');
+        let i = 0;
         const imgElement = item.querySelector('img');
         setInterval(() => {
-            currentIndex = (currentIndex + 1) % images[gameName].length;
-            const imgUrl = images[gameName][currentIndex];
+            i = (i + 1) % imagenes[titulo_juego].length;
+            const imgUrl = imagenes[titulo_juego][i];
             imgElement.src = imgUrl;
         }, 3000);
     });
