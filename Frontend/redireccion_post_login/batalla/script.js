ab225ee9b64dd7312e309a326a1deb5869759fba
notconra <cgarciap@fi.uba.ar>
2024-07-08 11:28:55 -0300

    arreglo de imagenes al hacer hover en seccion batalla

--- Frontend/redireccion_post_login/batalla/script.js
+++ Frontend/redireccion_post_login/batalla/script.js
@@ -1,42 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
     const items = document.querySelectorAll('.item_grid');
-    const hoverSound = new Audio('../../recursos_multimedia/hover_tienda.mp3');
+    const hover_sound = new Audio('../../recursos_multimedia/hover_tienda.mp3');
     items.forEach(item => {
         item.addEventListener('mouseenter', () => {
-            hoverSound.currentTime = 0;
-            hoverSound.play();
+            hover_sound.currentTime = 0;
+            hover_sound.play();
+
+            const img_element = item.querySelector('img');
+            const titulo_juego = item.querySelector('.titulo_juego').textContent.trim();
+            const imagenes = obtener_imagenes(titulo_juego);
+            if (imagenes.length > 0) {
+                img_element.src = imagenes[1];
+            }
+        });
+
+        item.addEventListener('mouseleave', () => {
+            const img_element = item.querySelector('img');
+            const titulo_juego = item.querySelector('.titulo_juego').textContent.trim();
+            const imagenes = obtener_imagenes(titulo_juego);
+
+            if (imagenes.length > 0) {
+                img_element.src = imagenes[0];
+            }
         });
     });
-});
-document.addEventListener("DOMContentLoaded", function() {
-    const items = document.querySelectorAll('.item_grid');
-    const imagenes = {
-        archivo1: [
-            "../../recursos_multimedia/imagenes_login/login_4.png",
-            "../../recursos_multimedia/imagenes_login/login_6.png"
-        ],
-        archivo2: [
-            "../../recursos_multimedia/imagenes_login/login_3.png",
-            "../../recursos_multimedia/imagenes_login/login_4.png"
-        ],
-        archivo3: [
-            "../../recursos_multimedia/imagenes_login/login_6.png",
-            "../../recursos_multimedia/imagenes_login/login_5.png"
-        ],
-        archivo4: [
-            "../../recursos_multimedia/imagenes_login/login_7.png",
-            "../../recursos_multimedia/imagenes_login/login_8.png"
-        ]
-    };
 
-    items.forEach(item => {
-        const titulo_juego = item.getAttribute('href').split('/').pop().replace('.html', '');
-        let i = 0;
-        const imgElement = item.querySelector('img');
-        setInterval(() => {
-            i = (i + 1) % imagenes[titulo_juego].length;
-            const imgUrl = imagenes[titulo_juego][i];
-            imgElement.src = imgUrl;
-        }, 3000);
-    });
+    function obtener_imagenes(titulo_juego) {
+        const imagenes = {
+            "PROBA TU CONOCIMIENTO!": [
+                "../../recursos_multimedia/30.png",
+                "../../recursos_multimedia/27.png"
+            ],
+            "DINO GAME": [
+                "../../recursos_multimedia/imagenes_login/login_3.png",
+                "../../recursos_multimedia/imagenes_login/login_4.png"
+            ],
+            "TATETI HELADO": [
+                "../../recursos_multimedia/imagenes_login/login_6.png",
+                "../../recursos_multimedia/imagenes_login/login_5.png"
+            ],
+            "TE ACORDAS DE TODOS?": [
+                "../../recursos_multimedia/imagenes_login/login_7.png",
+                "../../recursos_multimedia/imagenes_login/login_8.png"
+            ]
+        };
+
+        return imagenes[titulo_juego] || [];
+    }
 });
