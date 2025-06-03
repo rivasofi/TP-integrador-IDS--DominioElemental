2841edaf6b4a6972f96050c760e9ce3abe6d7a42
sofi <srivas@fi.uba.ar>
2025-06-02 22:36:38 -0300

    Fix render path

--- Backend/config.py
+++ Backend/config.py
@@ -1,4 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://intro_tp:intro_tp@localhost/dominio_elemental'
-
-#Esto sigue la estructura postgresql://nombre_usuario:tu_contraseña@localhost/nombre_basedatos, así que creen un usuario con ese nombre y contraseña, y la base de datos 
-#local con ese nombre también
+import os
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
